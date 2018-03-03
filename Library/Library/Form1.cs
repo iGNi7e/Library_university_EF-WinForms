451d1c2e66efd7edf3ede4c8ceadbcabc00bbb74
iGNiTe <ignarmezh@gmail.com>
2018-03-03 18:15:29 +0300

    Full Project

---
+++ Library/Library/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library
+{
+    public partial class Form1 : Form
+    {
+        MyLibEntities db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new MyLibEntities();
+            db.Books.Load();
+            dataGridView1.DataSource = db.Books.Local.ToBindingList();
+            
+        }
+
+        private void button1_Click(object sender,EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            List<Book> books = new List<Book>();
+            var group = db.Books.Where(p => p.Pages > 350);
+            foreach (var item in group)
+            {
+                
+                 books.Add(item);
+              
+            }
+            dataGridView1.DataSource = books;
+        }
+
+        private void button2_Click(object sender,EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = db.Books.Local.ToBindingList();
+        }
+
+        public int h = 10;
+
+        private void button3_Click(object sender,EventArgs e)
+        {
+            h = 12;
+            button4_Click(sender,e);
+            h = 10;
+            return;
+            List<Book> books = new List<Book>();
+      
+            dataGridView1.DataSource = null;
+
+
+            //System.Data.SqlClient.SqlParameter param = new System.Data.SqlClient.SqlParameter("@id","%1%");
+            //var book1 = db.Database.SqlQuery<Book>("SELECT * FROM db.Book WHERE Id",1);
+            //foreach (var b in book1)
+            //    books.Add(b);
+
+            var result1 = "SELECT * FROM Title WHERE Count >= {0}";
+            h = 10;
+            var query = db.Database.SqlQuery<Book>(result1,h);
+
+            dataGridView1.DataSource = books;
+        }
+
+        public void button4_Click(object sender,EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            List<Book> books = new List<Book>();
+            var group = from c in db.Books
+                        where c.Count < h
+                        select c;
+            foreach (var item in group)
+            {
+
+                books.Add(item);
+
+            }
+            dataGridView1.DataSource = books;
+        }
+
+        private void button5_Click(object sender,EventArgs e)
+        {
+            Form2 tmForm = new Form2();
+            DialogResult result = tmForm.ShowDialog(this);
+
+            if (result == DialogResult.Cancel)
+                return;
+
+            Book book = new Book();
+            book.Author = tmForm.textBox1.Text;
+            book.Title = tmForm.textBox2.Text;
+            book.Pages = Convert.ToInt32( tmForm.textBox3.Text);
+            book.Count = Convert.ToInt32(tmForm.textBox4.Text);
+
+            int max = db.Books.Count();
+            book.Id = max;
+            db.Books.Add(book);
+            db.SaveChanges();
+            MessageBox.Show("Новый объект добавлен");
+        }
+
+        private void button6_Click(object sender,EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int index = dataGridView1.SelectedRows[0].Index;
+                int id = 0;
+                bool converted = Int32.TryParse(dataGridView1[0,index].Value.ToString(),out id);
+                if (converted == false)
+                    return;
+
+                Book user = db.Books.Find(id);
+                db.Books.Remove(user);
+                db.SaveChanges();
+
+                MessageBox.Show("Объект удален");
+            }
+        }
+
+        private void button7_Click(object sender,EventArgs e)
+        {
+
+            dataGridView1.DataSource = null;
+            List<Book> books = new List<Book>();
+            var groups = db.Books.GroupBy(p => p.Title);
+            foreach (var g in groups)
+            {
+                foreach (var p in g)
+                    books.Add(p);
+            }
+            dataGridView1.DataSource = books;
+        }
+
+        private void button8_Click(object sender,EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            List<Book> books = new List<Book>();
+            var groups = from p in db.Books
+                         group p by p.Author;
+            foreach (var g in groups)
+            {
+                foreach (var p in g)
+                    books.Add(p);
+            }
+            dataGridView1.DataSource = books;
+        }
+    }
+}
---
+++ Library/Library/Form2.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Library
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(58, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(58, 89);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(58, 115);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(69, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 234);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.TextBox textBox3;
+        public System.Windows.Forms.TextBox textBox4;
+        public System.Windows.Forms.Button button1;
+    }
+}
